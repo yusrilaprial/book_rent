2dd0ed9e98823927e2ad12676f7585a2a880c8c4
yusrilaprial <myusrila23@gmail.com>
2022-11-17 15:01:22 +0700

    membuat fitur login logout dan middleware admin client

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function register()
+    {
+        return view('register');
+    }
+
+    public function authenticating(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        // 1. cek login valid
+        if (Auth::attempt($credentials)) {
+            // 2. cek user status = active
+            if (Auth::user()->status != 'active') {
+                Session::flash('status', 'warning');
+                Session::flash('message', 'Your Account is not active yet. Please contact admin!');
+                return redirect('/login');
+            }
+
+            $request->session()->regenerate();
+
+            if (Auth::user()->role_id == 1) {
+                return redirect('dashboard');
+            }
+
+            if (Auth::user()->role_id == 2) {
+                return redirect('profile');
+            }
+        }
+
+        Session::flash('status', 'danger');
+        Session::flash('message', 'Login Invalid');
+        return redirect('/login');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/login');
+    }
+}
---
+++ resources/views/login.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rental Buku | Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<style>
+    .main {
+        height: 100vh;
+        box-sizing: border-box;
+    }
+
+    .login-box {
+        width: 500px;
+        border: solid 1px;
+        padding: 10px;
+    }
+
+    form div {
+        margin-bottom: 15px;
+    }
+</style>
+
+<body>
+    <div class="main d-flex justify-content-center align-items-center">
+        <div class="login-box">
+            @if (session('status'))
+                <div class="alert alert-{{ session('status') }} text-center" role="alert">
+                    {{ session('message') }}
+                </div>
+            @endif
+            <form action="" method="post">
+                @csrf
+                <div>
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" name="username" id="username" class="form-control" required>
+                </div>
+                <div>
+                    <label for="password" class="form-label">password</label>
+                    <input type="password" name="password" id="password" class="form-control" required>
+                </div>
+                <div>
+                    <button type="submit" class="btn btn-primary form-control">Login</button>
+                </div>
+                <div class="text-center">
+                    <a href="/register">Sign Up</a>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
